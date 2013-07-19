ccbde41521a695e8321ed6cb49b7d24407c079d1
cconrose <cconrose@knights.ucf.edu>
2013-07-19 16:22:53 -0400

    Added basic functions to ARA, PA, RA, VPA

--- src/com/rt/ui/RunningActivity.java
+++ src/com/rt/ui/RunningActivity.java
@@ -1,7 +1,15 @@
 package com.rt.ui;
-
-import java.util.*;
+import com.rt.runtime.LocationMonitor;
 
 public class RunningActivity{
+	LocationMonitor lm = new LocationMonitor();
+	
+	public void onCreate(){
+		
+	}
+	
+	public void onClick(){
+		
+	}
 	
 }
---
+++ src/com/rt/ui/ViewPastActivity.java
@@ -0,0 +1,15 @@
+package com.rt.ui;
+import com.rt.runtime.MetricsAggregator;
+
+public class ViewPastActivity{
+	MetricsAggregator ma = new MetricsAggregator();
+	
+	public void onCreate(){
+		
+	}
+	
+	public void onClick(){
+		
+	}
+	
+}
