8b5b992569fb19d7a8e89fa56e050bc6010a837e
Andrew Foran <aforan@knights.ucf.edu>
2013-07-19 13:18:17 -0400

    Added more boilerplate to core

--- src/com/rt/core/MapDataManager.java
+++ src/com/rt/core/MapDataManager.java
@@ -1,7 +1,43 @@
 package com.rt.core;
 
 public class MapDataManager {
+	private ArrayList<Waypoint> waypoints;
+	private ArrayList<Leg> legs;
+	public Position currentPos;
+
 	public MapDataManager() {
-		
+
+	}
+
+	public ArrayList<Waypoint> getWaypoints() {
+
+	}
+
+	public ArrayList<Legs> getLegs() {
+
+	}
+
+	public void addWaypoint(Waypoint w) {
+
+	}
+
+	public boolean removeWaypoint(Waypoint w) {
+
+	}
+
+	public MapElement getElement(Position p) {
+
+	}
+
+	public Waypoint getWaypoint(Position p)  {
+
+	}
+
+	public Leg getLeg(Position p) {
+
+	}
+
+	public void updatePosition() {
+
 	}
 }
