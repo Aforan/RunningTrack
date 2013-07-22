c302695422d500344bd2cf9632419ad8a88ae5a3
Andy <andyasp@knights.ucf.edu>
2013-07-21 22:28:53 -0400

    Basic map functionality

---
+++ src/com/rt/ui/PlanChoosingActivity.java
@@ -0,0 +1,26 @@
+package com.rt.ui;
+
+import com.rt.R;
+import com.rt.R.layout;
+import com.rt.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PlanChoosingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_plan_choosing);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.plan_choosing, menu);
+		return true;
+	}
+
+}
--- src/com/rt/ui/RunningActivity.java
+++ src/com/rt/ui/RunningActivity.java
@@ -1,15 +1,106 @@
 package com.rt.ui;
 import com.rt.runtime.LocationMonitor;
 
-public class RunningActivity{
-	LocationMonitor lm = new LocationMonitor();
+import android.os.Bundle;
+import android.app.Activity;
 
-	public void onCreate(){
+import com.rt.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.Location;
+import android.location.LocationManager;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
+
+public class RunningActivity extends FragmentActivity
+implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+	
+	private GoogleMap map;
+	private LocationManager lm;
+	private LocationClient lc;
+	private static final LocationRequest REQUEST = LocationRequest.create()
+		      .setInterval(5000)         // 5 seconds
+		      .setFastestInterval(16)    // 16ms = 60fps
+		      .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_running);
 	}
+	
+	@Override
+	  protected void onResume() {
+	    super.onResume();
+	    setUpMapIfNeeded();
+	    map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+	    setUpLocationClientIfNeeded();
+	    lc.connect();
+   }
+	
+   void setUpMapIfNeeded(){
+	   if(map == null){
+		// Try to obtain the map from the SupportMapFragment.
+		      map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+		             .getMap();
+		      // Check if we were successful in obtaining the map.
+		      if (map != null) {
+		        map.setMyLocationEnabled(true);
+		      }
+	   }
+   }
+   
+   private void setUpLocationClientIfNeeded() {
+	    if (lc == null) {
+	      lc = new LocationClient(
+	          getApplicationContext(),
+	          this,  // ConnectionCallbacks
+	          this); // OnConnectionFailedListener
+	    }
+   }
 
-	public void onClick(){
+@Override
+public void onLocationChanged(Location location) {
+	//tracing of user here?
+	
+}
 
-	}
+@Override
+public void onConnectionFailed(ConnectionResult result) {
+	//do nothing
+	
+}
+
+@Override
+public void onConnected(Bundle connectionHint) {
+	lc.requestLocationUpdates(
+	        REQUEST,
+	        this);
+	
+}
+
+@Override
+public void onDisconnected() {
+	//do nothing
+	
+}
+	
+	
+
 
 }
